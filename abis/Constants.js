6231cfbcea28b59437cd7117919ed821b4cfec0b
ZAMBUR <136861183+SNABUR@users.noreply.github.com>
2024-11-14 14:33:42 -0500

    Update Constants.js

--- abis/Constants.js
+++ abis/Constants.js
@@ -1,6 +1,6 @@
 const UniswapV2Factory = require('./UniswapV2Factory.json');
 const GoldengNFT = require('./GoldengNFT.json');
-const MemeCoin = require('./MemeCoin.json');
+const MemeCoin = require('./MemeCoinGG.json');
 
 const contractABI_UNISWAP_FACTORY_V2 = UniswapV2Factory.abi;
 const contractABI_NFT_COLLECTION = GoldengNFT.abi;
